a4d7a945a03dc2babd221eacdbad0f30e5f6283f
YOUR NAME <YOUR_GITHUB_EMAIL@HERE.COM>
2021-03-24 15:11:25 +0100

    finalized

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ import time
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras import regularizers
+from tensorflow.keras.callbacks import ModelCheckpoint
+import keras
 
 app = Flask(__name__)
 
@@ -16,29 +21,38 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/single-post.html', methods=['GET'])
-def single_post():
-    return render_template('single-post.html')
 
-@app.route('/data_exploration.html', methods=['GET'])
-def data_exploration():
-    return render_template('data_exploration.html')
 
-@app.route('/new_test.html', methods=['GET','POST'])
-def new_test():
-    upload_dir = os.path.join("..","img")
+@app.route('/', methods=['POST'])
+def models():
+
+
     if request.method == 'POST':
-        # save the single "profile" file
-        profile = request.files['profile']
-        profile.save(os.path.join(uploads_dir, "test_image.png"))
+        if request.form.get("lr_button"):
+            with open("linearmodel.pkl","rb") as f:
+                lr=pickle.load(f)
+            rooms = int(request.form['rooms'])
+            area = int(request.form['area'])
+            nb_lots = int(request.form['lots'])
+            prediction=lr.predict(np.array([nb_lots,area,rooms]).reshape(1,3))
+            return render_template('index.html',prediction=prediction)
+        elif request.form.get("nn_button"):
+            my_model = tf.keras.models.load_model('my_model')
+            rooms = int(request.form['rooms2'])
+            area = int(request.form['area2'])
+            nb_lots = int(request.form['lots2'])
+            prediction=my_model.predict(np.array([nb_lots,area,rooms]).reshape(1,3))
+            return render_template('index.html',prediction2=prediction)
+        elif request.form.get("rent_button"):
+            with open("rent_model.pkl","rb") as f:
+                    lr=pickle.load(f)
+            rooms = int(request.form['rooms3'])
+            district = int(request.form['district3'])
+            area = int(request.form['area3'])
+            prediction=lr.predict(np.array([district,area,rooms]).reshape(1,3))
+            ten_year_revenue=12*10*area*prediction
+            return render_template('index.html',ten_year_revenue=ten_year_revenue)
 
-        return redirect(url_for('upload'))
-    return render_template('new_test.html')
-
-@app.route('/prediction.html', methods=['GET'])
-def prediction():
-
-    return render_template('prediction.html')
 
 
 
